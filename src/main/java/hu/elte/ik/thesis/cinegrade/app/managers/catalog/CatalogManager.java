dcb48e3aa018abce6b9d05067bfa3ecbef1f333a
Hogyi1 <hogyor.peter@gmail.com>
2026-09-24 09:47:28 +0200

    Implement catalog retrieval and enhance MainMenuController; add filtering and sorting functionality for catalogs

--- src/main/java/hu/elte/ik/thesis/cinegrade/app/managers/catalog/CatalogManager.java
+++ src/main/java/hu/elte/ik/thesis/cinegrade/app/managers/catalog/CatalogManager.java
@@ -2,6 +2,7 @@ package hu.elte.ik.thesis.cinegrade.app.managers.catalog;
 
 import hu.elte.ik.thesis.cinegrade.domain.catalog.Catalog;
 import hu.elte.ik.thesis.cinegrade.infra.services.database.RecentCatalogService;
+import javafx.collections.ObservableList;
 
 public class CatalogManager {
 
@@ -16,5 +17,9 @@ public class CatalogManager {
 
     public void deleteCatalog(Catalog catalog) {
 
+    }
+
+    public ObservableList<Catalog> getCatalogs() {
+        return null;
     }
 }
--- src/main/java/hu/elte/ik/thesis/cinegrade/javafx/ui/catalog/MainMenuController.java
+++ src/main/java/hu/elte/ik/thesis/cinegrade/javafx/ui/catalog/MainMenuController.java
@@ -1,4 +1,103 @@
 package hu.elte.ik.thesis.cinegrade.javafx.ui.catalog;
 
+import hu.elte.ik.thesis.cinegrade.app.managers.catalog.CatalogManager;
+import hu.elte.ik.thesis.cinegrade.domain.catalog.Catalog;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+import java.util.Comparator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class MainMenuController {
+
+    @FXML
+    private Button newProjectButton;
+    @FXML
+    private Button openProjectButton;
+    @FXML
+    private HBox homeNav;
+    @FXML
+    private HBox socialNav;
+
+    @FXML
+    private Label welcomeLabel;
+    @FXML
+    private TextField searchField;
+    @FXML
+    private ListView<Catalog> catalogListView;
+
+    @FXML
+    private HBox sizeFilter;
+    @FXML
+    private HBox nameFilter;
+    @FXML
+    private HBox recentFilter;
+    @FXML
+    private HBox creationFilter;
+
+    @FXML
+    private ImageView sortNameIcon;
+    @FXML
+    private ImageView sortSizeIcon;
+    @FXML
+    private ImageView sortRecentIcon;
+    @FXML
+    private ImageView sortCreationIcon;
+
+    private final CatalogManager catalogManager;
+    private ObservableList<Catalog> catalogObservableList;
+    private FilteredList<Catalog> filteredList;
+    private SortedList<Catalog> sortedList;
+    private final ObjectProperty<Catalog> selectedProperty = new SimpleObjectProperty<>();
+
+    private final Consumer<Catalog> removeRecentConsumer;
+    private final Consumer<Catalog> deleteConsumer;
+
+    public MainMenuController(CatalogManager catalogManager) {
+        this.catalogManager = catalogManager;
+
+        deleteConsumer = catalogManager::deleteCatalog;
+        removeRecentConsumer = catalogManager::removeFromRecent;
+    }
+
+    @FXML
+    public void initialize() {
+        catalogObservableList = catalogManager.getCatalogs();
+
+        filteredList = new FilteredList<>(catalogObservableList, pr -> true);
+        sortedList = new SortedList<>(filteredList, Comparator.comparing(Catalog::getCatalogName));
+
+        catalogListView.setCellFactory(lvC -> {
+            CatalogViewItem viewItem = new CatalogViewItem(removeRecentConsumer, deleteConsumer);
+            return viewItem;
+        });
+
+        catalogListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != selectedProperty.getValue() && newValue != null) {
+                selectedProperty.set(newValue);
+            }
+        });
+
+        searchField.setOnAction(e -> {
+            String text = searchField.getText().trim().toLowerCase();
+
+            filteredList.setPredicate(catalog -> text.isEmpty() || catalog.getCatalogName().contains(text));
+        });
+
+        catalogListView.setItems(sortedList);
+    }
 }
